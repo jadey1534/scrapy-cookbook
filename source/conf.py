8d490d19c5d6e1bb7353249aeeabbbe6f2b29ec9
jadey1534 <103307523+jadey1534@users.noreply.github.com>
2022-04-12 11:44:35 +0800

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -15,8 +15,8 @@
 # sys.path.insert(0, os.path.abspath('.'))
 import sphinx_rtd_theme
 from recommonmark.parser import CommonMarkParser
-html_theme = "alabaster"
-html_theme_path = [alabaster.get_html_theme_path()]
+html_theme = "sphinx_rtd_theme"
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 source_parsers = {
     '.md': CommonMarkParser,
@@ -62,7 +62,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+# html_theme = 'alabaster'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,

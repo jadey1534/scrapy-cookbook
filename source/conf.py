c9584fd3d41fb5714f72dfdd2f11b4d370d25821
jadey1534 <103307523+jadey1534@users.noreply.github.com>
2022-04-12 11:47:05 +0800

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -39,6 +39,10 @@ release = '1.0'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.mathjax',
+    'recommonmark',
 ]
 
 # Add any paths that contain templates here, relative to this directory.

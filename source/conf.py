3a18bb409ab5de5a152345122c66af734fdd4b23
jadey1534 <103307523+jadey1534@users.noreply.github.com>
2022-04-16 11:33:02 +0800

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -66,7 +66,8 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-# html_theme = 'alabaster'
+html_theme = "bizstyle"
+# html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,

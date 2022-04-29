43e16780b73ee280114683d8a9b123eb52cf1318
jadey1534 <103307523+jadey1534@users.noreply.github.com>
2022-04-29 15:23:15 +0800

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -70,7 +70,7 @@ exclude_patterns = []
 html_theme = "sphinx_rtd_theme"
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 html_static_path = ['_static']
-html_logo = "_static/Fnetlink.png"
+html_logo = "_static/Fnetlink1.png"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
